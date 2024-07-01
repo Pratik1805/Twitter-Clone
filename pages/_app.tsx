e3f3435b775a08fd7d13e452adef9718146843af
Pratik1805 <pratikpandey0532@gmail.com>
2024-07-01 16:16:36 +0530

    Created Modal Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/Components/Layout";
+import Modal from "@/Components/modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <>
+      {/* <Modal actionLabel="Submit" isOpen title="Test Modal"/> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
